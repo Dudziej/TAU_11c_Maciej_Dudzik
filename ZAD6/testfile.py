536d3a94bee99cd549bcfb4ff4d51a2b89d8d8f4
Dudziej <46353850+Dudziej@users.noreply.github.com>
2021-04-22 15:57:27 +0200

    Zad 6 Code Upload

---
+++ ZAD6/testfile.py
@@ -0,0 +1,125 @@
+import pytest
+from main import Komendy
+
+
+class TestClass:
+    def test_komendy_bstart(self):
+        hasattr(Komendy, 'komendy')
+
+    def test_komendy_astart(self):
+        sysname = "Windows"
+        komendy = Komendy(sysname)
+        hasattr(komendy, 'komendy')
+
+    def test_sysname_windows(self):
+        sysname = "Windows"
+        komendy = Komendy(sysname)
+        assert komendy.sysname == sysname
+
+    def test_sysname_linux(self):
+        sysname = "Linux"
+        komendy = Komendy(sysname)
+        assert komendy.sysname == sysname
+
+    def test_sysname_bad(self):
+        sysname = "bad"
+        with pytest.raises(Exception):
+            Komendy(sysname)
+
+    def test_sysname_liczba(self):
+        sysname = 256
+        with pytest.raises(Exception):
+            Komendy(sysname)
+
+    def test_sysname_tablica(self):
+        sysname = [2, 1, 3, 7]
+        with pytest.raises(Exception):
+            Komendy(sysname)
+
+    def test_sysname_none(self):
+        sysname = None
+        with pytest.raises(Exception):
+            Komendy(sysname)
+
+    def test_sysname_float(self):
+        sysname = 4.20
+        with pytest.raises(Exception):
+            Komendy(sysname)
+
+
+class TestClassWindows:
+    def test_win_exit(self):
+        komenda = Komendy("Windows")
+        result = komenda.help('exit')
+        assert result == 'zamyka cmd'
+
+    def test_win_bad_kom(self):
+        komenda = Komendy("Windows")
+        result = komenda.help('pieniadzezalas')
+        assert result is None
+
+    def test_win_bad_kom_arg(self):
+        komenda = Komendy("Windows")
+        result = komenda.help('gdzie -sa')
+        assert result is None
+
+    def test_win_cd_arg(self):
+        komenda = Komendy("Windows")
+        result = komenda.help('cd ..')
+        assert result == 'wyjscie z folderu'
+
+    def test_win_cd_arg2(self):
+        komenda = Komendy("Windows")
+        result = komenda.help('cd Desktop')
+        assert result == '{argument} wejscie do folderu'
+
+    def test_win_cd_arg_bad_arg(self):
+        komenda = Komendy("Windows")
+        result = komenda.help('cd .. wololo')
+        assert result == 'wyjscie z folderu'
+
+    def test_win_none(self):
+        komenda = Komendy("Windows")
+        result = komenda.help(None)
+        assert result is None
+
+
+class TestClassLinux:
+
+    def test_linux_rm_arg(self):
+        komenda = Komendy("Linux")
+        result = komenda.help('rm -f')
+        assert result == 'usunięcie plików zabezpieczonych'
+
+    def test_linux_ls(self):
+        komenda = Komendy("Linux")
+        result = komenda.help('ls')
+        assert result == 'wyświetla pliki i katalogi'
+
+    def test_linux_bad_kom_arguments(self):
+        komenda = Komendy("Linux")
+        result = komenda.help('my -nameis')
+        assert result is None
+
+    def test_linux_bad_kom_arg_arg(self):
+        komenda = Komendy("Linux")
+        result = komenda.help('my -nameis Jeff')
+        assert result is None
+
+    def test_linux_int(self):
+        komenda = Komendy("Linux")
+        result = komenda.help(420)
+        assert result is None
+
+    def test_linux_empty(self):
+        komenda = Komendy("Linux")
+        result = komenda.help("")
+        assert result is None
+
+    def test_linux_pwd(self):
+        komenda = Komendy("Linux")
+        result = komenda.help("pwd")
+        assert result == 'wyświetla aktualną ścieżkę'
+
+
+
